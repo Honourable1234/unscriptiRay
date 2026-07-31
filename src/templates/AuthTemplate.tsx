c5c43e16e86e66b1e4d005b90f663b85980227a8
TOLU-MICH <michealtolu2003@gmail.com>
2026-07-31 13:11:52 +0100

    feat: enhance authentication pages with new links and improved layout

--- src/templates/AuthTemplate.tsx
+++ src/templates/AuthTemplate.tsx
@@ -4,18 +4,18 @@ export const AuthTemplate = (props: {
   children: React.ReactNode;
 }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between bg-black-80">
-      <header className=" flex h-15 w-full items-center justify-center bg-black-100 md:h-25">
+    <div className="flex h-screen flex-col bg-black-80">
+      <header className="flex h-15 w-full shrink-0 items-center justify-center bg-black-100 md:h-25">
         <div className="relative h-full w-15 md:w-25.5">
           <Image src="/General/Unscripti-logo.png" fill alt="Unscripti Logo" sizes="(max-width: 768px) 60px, 102px" className="h-full object-contain" />
         </div>
       </header>
-      <div className="flex w-full items-center justify-center p-3 sm:p-4">
+      <div className="flex w-full flex-1 items-center-safe justify-center overflow-y-auto p-3 pt-6 sm:p-4 sm:pt-8">
         <div className="flex w-full max-w-129.5 flex-col rounded-3xl border border-black-40 bg-black-60 p-4 shadow-sm md:p-6">
           {props.children}
         </div>
       </div>
-      <footer className="flex h-15 w-full items-center justify-center bg-black-100 md:h-25">
+      <footer className="flex h-15 w-full shrink-0 items-center justify-center bg-black-100 md:h-25">
         <p className="text-sm font-semibold text-white">
           Copyright©
           {new Date().getFullYear()}
